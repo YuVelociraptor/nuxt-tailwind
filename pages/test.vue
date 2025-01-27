1c64e9cb72c568045eaa1545c6c019cf09197f81
Yusuke Shinohara <complex.alpha.cxa@gmail.com>
2025-01-27 17:15:45 +0900

    Create test.vue

---
+++ pages/test.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <h2 class="text-xl font-bold underline">
+    Hello world!
+  </h2>
+
+  <div class="grid grid-cols-3 gap-4">
+    <div class="bg-red-500 p-10">1</div>
+    <div class="bg-green-300 p-20">2</div>
+    <div class="bg-blue-400 p-4">3</div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
