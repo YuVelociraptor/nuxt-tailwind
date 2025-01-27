c25b75a044e32a7109cb8155a7632a0f6d5a895b
Yusuke Shinohara <complex.alpha.cxa@gmail.com>
2025-01-27 17:15:43 +0900

    Create index.vue

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <h1>
+    This is a test page.
+  </h1>
+</template>
+
+<style scoped>
+
+</style>
