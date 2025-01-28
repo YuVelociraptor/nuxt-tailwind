73590439d7976fcf0c0ebdddeeb174a587ff64d4
Yusuke Shinohara <complex.alpha.cxa@gmail.com>
2025-01-28 22:29:54 +0900

    Update navbars.vue

--- pages/navbars.vue
+++ pages/navbars.vue
@@ -24,15 +24,6 @@
   <p>ハンバーガーメニュー（展開時）左</p>
   <Navbar2 />
 
-  <h1 class="text-3xl">3</h1>
-  <h2 class="text-2xl">画面幅 広</h2>
-  <p>ロゴ左</p>
-  <p>メニュー左</p>
-  <h2 class="text-2xl">画面幅 狭</h2>
-  <p>ロゴ左</p>
-  <p>ハンバーガーボタン右</p>
-  <p>ハンバーガーメニュー（展開時）左</p>
-  <Navbar3 />
 
 </template>
 
