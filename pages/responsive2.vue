72bf2ee96d61838c1d70bd292e4dedf23b108787
Yusuke Shinohara <complex.alpha.cxa@gmail.com>
2025-01-28 23:08:36 +0900

    text

--- pages/responsive2.vue
+++ pages/responsive2.vue
@@ -3,26 +3,31 @@
 </script>
 
 <template>
+  <h1 class="text-4xl font-bold">sm</h1>
   <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
     <div class="bg-blue-200 p-4">1</div>
     <div class="bg-green-200 p-4">2</div>
     <div class="bg-red-200 p-4">3</div>
   </div>
+  <h1 class="text-4xl font-black">md</h1>
   <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
     <div class="bg-blue-200 p-4">1</div>
     <div class="bg-green-200 p-4">2</div>
     <div class="bg-red-200 p-4">3</div>
   </div>
+  <h1 class="text-4xl font-bold text-red-600">lg</h1>
   <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
     <div class="bg-blue-200 p-4">1</div>
     <div class="bg-green-200 p-4">2</div>
     <div class="bg-red-200 p-4">3</div>
   </div>
+  <h1 class="text-4xl font-thin text-green-600">xl</h1>
   <div class="grid grid-cols-1 xl:grid-cols-3 gap-4">
     <div class="bg-blue-200 p-4">1</div>
     <div class="bg-green-200 p-4">2</div>
     <div class="bg-red-200 p-4">3</div>
   </div>
+  <h1 class="text-4xl font-bold text-blue-600 italic">2xl</h1>
   <div class="grid grid-cols-1 2xl:grid-cols-3 gap-4">
     <div class="bg-blue-200 p-4">1</div>
     <div class="bg-green-200 p-4">2</div>
