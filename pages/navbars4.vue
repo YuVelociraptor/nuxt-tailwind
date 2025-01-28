32174afac30c1cf39843437d244504351a633b10
Yusuke Shinohara <complex.alpha.cxa@gmail.com>
2025-01-28 22:31:32 +0900

    Create navbars4.vue

---
+++ pages/navbars4.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+  <h1 class="text-3xl">7</h1>
+  <h2 class="text-2xl">画面幅 広</h2>
+  <p>ロゴ左</p>
+  <p>メニュー左</p>
+  <h2 class="text-2xl">画面幅 狭</h2>
+  <p>ロゴ左</p>
+  <p>ハンバーガーボタン右</p>
+  <p>ハンバーガーメニュー（展開時）左</p>
+  <Navbar7 />
+
+</template>
+
+<style scoped>
+
+</style>
