ec2e4d76b9672991affe509ecb37106cb196e127
Yusuke Shinohara <complex.alpha.cxa@gmail.com>
2025-01-28 22:21:33 +0900

    Update navbars.vue

--- pages/navbars.vue
+++ pages/navbars.vue
@@ -4,15 +4,35 @@
 
 <template>
 
-  <h1>1</h1>
-  <h2>画面幅 広</h2>
+  <h1 class="text-3xl">1</h1>
+  <h2 class="text-2xl">画面幅 広</h2>
   <p>ロゴ左</p>
   <p>メニュー右</p>
-  <h2>画面幅 狭</h2>
+  <h2 class="text-2xl">画面幅 狭</h2>
   <p>ロゴ左</p>
   <p>ハンバーガーボタン右</p>
   <p>ハンバーガーメニュー（展開時）左</p>
   <Navbar1 />
+
+  <h1 class="text-3xl">2</h1>
+  <h2 class="text-2xl">画面幅 広</h2>
+  <p>ロゴ左</p>
+  <p>メニュー左</p>
+  <h2 class="text-2xl">画面幅 狭</h2>
+  <p>ロゴ左</p>
+  <p>ハンバーガーボタン右</p>
+  <p>ハンバーガーメニュー（展開時）左</p>
+  <Navbar2 />
+
+  <h1 class="text-3xl">3</h1>
+  <h2 class="text-2xl">画面幅 広</h2>
+  <p>ロゴ左</p>
+  <p>メニュー左</p>
+  <h2 class="text-2xl">画面幅 狭</h2>
+  <p>ロゴ左</p>
+  <p>ハンバーガーボタン右</p>
+  <p>ハンバーガーメニュー（展開時）左</p>
+  <Navbar3 />
 
 </template>
 
